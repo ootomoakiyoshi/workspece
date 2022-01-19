48e63c198a40022b1ce79e151bdce3c3d79d7f3e
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2022-01-19 16:05:25 +0900

    変数

---
+++ TestProject/src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+  public static void main (String[] args){
+    int  age;
+    age = 30;
+    System.out.println(age);
+
+  }
+}
+
+// 変数ageの箱を用意
+// 変数の箱へ30を入れる
+// System.out.printlnにて出力
