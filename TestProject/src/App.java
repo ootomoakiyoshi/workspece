1213d168a556b200a0accdcf8eb92961f9822b5e
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2022-01-19 16:01:42 +0900

    add

--- TestProject/src/App.java
+++ TestProject/src/App.java
@@ -1,6 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        // Hello, World!が出力される。
+        int version;
+        version = 1 + 5;
+        System.out.println("日記ソフト");
+        System.out.println("ver" + version);
+        System.out.println("まだ開発注です");
+        System.out.println("終了します");
     }
 }
