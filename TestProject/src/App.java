1a72e04127f7e773a31d30fb0d64605d984a7567
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2022-01-25 18:37:45 +0900

    add

--- TestProject/src/App.java
+++ TestProject/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
         System.out.println("文字出力");
+        System.out.println(5 + 5);
     }
 }
