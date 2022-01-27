dd1edc31123d6a5ecf74c442045334a983bfd0e3
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2022-01-27 17:46:03 +0900

    計算式

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -6,6 +6,15 @@ public class Test {
     // 変数ageの箱を用意
     // 変数の箱へ30を入れる
     // System.out.printlnにて出力
+
+    // 計算式
+    int a;
+    int b;
+    a = 20;
+    b = a + 5;
+
+    System.out.println(a);
+    System.out.println(b);
   }
 }
 
