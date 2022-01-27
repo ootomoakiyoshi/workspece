cb34cb5140b27400f860e92bf049d44f63285407
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2022-01-27 17:41:44 +0900

    add

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -3,10 +3,9 @@ public class Test {
     int  age;
     age = 30;
     System.out.println("私の年齢は" + age + "歳です");
-
+    // 変数ageの箱を用意
+    // 変数の箱へ30を入れる
+    // System.out.printlnにて出力
   }
 }
 
-// 変数ageの箱を用意
-// 変数の箱へ30を入れる
-// System.out.printlnにて出力
