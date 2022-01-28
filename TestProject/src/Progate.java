2cafb03014a9408c083dad2426e3bf45dba7be3c
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2022-01-28 15:53:26 +0900

    演習問題

---
+++ TestProject/src/Progate.java
@@ -0,0 +1,14 @@
+public class Progate {
+  public static void main(String[] args){
+    int number = 8;
+    // 変数に7を掛けて変数の上書き
+    number *= 7;
+    // 変数の出力
+    System.out.println(number);
+
+    // 変数に1を足す。(省略で)
+    number++ ;
+    System.out.println(number);
+
+  }
+}
