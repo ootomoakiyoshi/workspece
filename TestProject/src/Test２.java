e4acac581a31f4709f85b76ab09d8885ccacb2f0
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2022-01-31 20:26:07 +0900

    add

---
+++ TestProject/src/Test２.java
@@ -0,0 +1,9 @@
+public class Test２ {
+  public static void main(String[] args) {
+      // 変数を定義し、値を比較した結果を代入してください
+      boolean bool = 3 * 9 == 27;
+      // 変数boolの値を出力してください
+      System.out.println(bool);
+      
+  }
+}
