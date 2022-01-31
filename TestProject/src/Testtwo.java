ac2a2699c5c29f2a8957f4e663bfce83f2b1edb5
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2022-01-31 21:07:44 +0900

    出力方法

---
+++ TestProject/src/Testtwo.java
@@ -0,0 +1,8 @@
+public class Testtwo {
+  public static void main(String[] args) {
+    String name ="大友" ;
+    System.out.print("私の名前は");
+    System.out.print(name);
+    System.out.println("です");
+  }
+}
--- TestProject/src/Test２.java
+++
@@ -1,9 +0,0 @@
-public class Test２ {
-  public static void main(String[] args) {
-      // 変数を定義し、値を比較した結果を代入してください
-      boolean bool = 3 * 9 == 27;
-      // 変数boolの値を出力してください
-      System.out.println(bool);
-      
-  }
-}
