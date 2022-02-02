68a6012eb230efaff7859145229ea36d04e29a69
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2022-02-02 17:16:50 +0900

    if分

---
+++ TestProject/src/Writings.java
@@ -0,0 +1,13 @@
+public class Writings {
+   public static void main(String[] args) {
+    int x = 5;
+    if(x > 3){
+      System.out.println("xは3より大きい");
+      // xは3より大きい為、出力される。
+      }
+      if(x > 7){
+        System.out.println("xは7より大きい");
+        // xは7より小さい為、出力されない。
+      }
+    }
+  }
