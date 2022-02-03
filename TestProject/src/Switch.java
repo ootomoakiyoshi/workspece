25ee6054703ffbaa7dfb17b18cd1d885624f6d96
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2022-02-03 15:23:55 +0900

    switch

---
+++ TestProject/src/Switch.java
@@ -0,0 +1,16 @@
+public class Switch {
+  public static void main(String[] args){
+    int n = 1;
+    switch (n){
+      case 1:
+      System.out.println("大吉");
+      break;
+      case 2:
+      System.out.println("吉");
+      break;
+      case 3:
+      System.out.println("中吉");
+      break;
+    }
+  }
+}
