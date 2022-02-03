e8ebae32955f53a200099aba161995201245b6d1
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2022-02-03 15:26:55 +0900

    インデント

--- TestProject/src/Switch.java
+++ TestProject/src/Switch.java
@@ -1,16 +1,16 @@
 public class Switch {
   public static void main(String[] args){
     int n = 1;
-    switch (n){
+    switch (n) {
       case 1:
-      System.out.println("大吉");
-      break;
+       System.out.println("大吉");
+       break;
       case 2:
-      System.out.println("吉");
-      break;
+       System.out.println("吉");
+       break;
       case 3:
-      System.out.println("中吉");
-      break;
+       System.out.println("中吉");
+       break;
     }
   }
 }
