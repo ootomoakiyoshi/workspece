99f0e77c32b9e78d071f48b94ab96a8ee0610a9f
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2022-02-04 18:25:35 +0900

    [Finish]4章

---
+++ TestProject/src/Chapter04.java
@@ -0,0 +1,10 @@
+public class Chapter04 {
+  public static void main(String[] args){
+    String name = "大友";
+    System.out.println(name);
+    name = "鈴木";
+    System.out.println(name);
+    final int number = 2;
+    System.out.println(number);
+  }
+}
