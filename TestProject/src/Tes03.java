bc263af86c63b69952fa4139488fb5bea4107489
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2022-04-12 20:50:37 +0900

    add

--- TestProject/src/Tes03.java
+++ TestProject/src/Tes03.java
@@ -1,9 +1,12 @@
 public class Tes03 {
-  public static void main(String [] args){
+  public static void main(String[] args) {
     int number = 10;
     System.out.println(number);
 
     number = number + 10;
+    System.out.println(number);
+
+    number += 10;
     System.out.println(number);
   }
 }
