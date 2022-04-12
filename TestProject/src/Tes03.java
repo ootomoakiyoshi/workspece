59e939cd34bf2ba80830f7a92f5c76d6e9671221
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2022-04-12 20:47:25 +0900

    変数

--- TestProject/src/Tes03.java
+++ TestProject/src/Tes03.java
@@ -1,7 +1,9 @@
 public class Tes03 {
   public static void main(String [] args){
-    System.out.println("大友");
+    int number = 10;
+    System.out.println(number);
 
-    System.out.println(9 + 41);
+    number = number + 10;
+    System.out.println(number);
   }
 }
