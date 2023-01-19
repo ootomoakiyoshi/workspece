c0972dc61d1186ee8da4f677d04a006cd8e11229
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2023-01-19 19:56:05 +0900

    add

--- TestProject/src/Chapter05.java
+++ TestProject/src/Chapter05.java
@@ -1,7 +1,10 @@
 public class Chapter05 {
-  public static void main(String[] args){
-    System.out.println("テスト");
-    System.out.println(5+9);
+
+  public static void main(String[] args) {
+    System.out.println("Hello");
+    System.out.println(5 + 10);
   }
 }
+
+
 
