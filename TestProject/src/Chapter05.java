639a5b8d649a3f92e4c9ba9e536a7b90d3ce7781
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2023-01-25 19:24:12 +0900

    add

--- TestProject/src/Chapter05.java
+++ TestProject/src/Chapter05.java
@@ -1,10 +1,23 @@
+import javax.xml.validation.Validator;
+
 public class Chapter05 {
 
   public static void main(String[] args) {
-    System.out.println("Hello");
-    System.out.println(5 + 10);
+    int val = 2 + 5;
+    System.out.println(val);
+
+    val++;
+    System.out.println(val);
+
+    val *= 5;
+    System.out.println(val);
+
+    boolean bool = (val == 50);
+    // 「==」を使用して、「valは50と等しい」の結果を代入
+    System.out.println(bool);
+
+    bool = (val < 50) && (val % 10 == 0);
+    System.out.println(bool);
+    // valは50より小さい」は、関係（比較）演算子の「<」を使用し、「valを10で割った余りが0」の余りの計算は、算術演算子の「%」を使用。
   }
 }
-
-
-
